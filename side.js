dff40e21b8258bd4e86d447bf437a3116c5d55ef
Erencan KARADAĞ <64350262+ekaradag14@users.noreply.github.com>
2024-11-11 21:49:10 +0300

    Update side.js

--- side.js
+++ side.js
@@ -2,14 +2,14 @@
 if (localStorage.getItem('rag-client') === 'true') {
   // Load the Botpress inject script
   var script1 = document.createElement('script');
-  script1.src = 'https://cdn.botpress.cloud/webchat/v2.1/inject.js';
+  script1.src = 'https://cdn.botpress.cloud/webchat/v2.2/inject.js';
   script1.defer = true;
   document.head.appendChild(script1);
 
   // Load the Botpress config script after the inject script has loaded
   script1.onload = function() {
     var script2 = document.createElement('script');
-    script2.src = 'https://mediafiles.botpress.cloud/17a32590-61ca-44a6-89b7-ab13d83f5a61/webchat/v2.1/config.js';
+    script2.src = 'https://files.bpcontent.cloud/2024/10/07/12/20241007120304-O0RURIDS.js';
     script2.defer = true;
     document.head.appendChild(script2);
   };
